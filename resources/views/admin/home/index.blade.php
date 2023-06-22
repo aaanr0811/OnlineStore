1e2ebc26c9fd1a92e416cfdec0c71cf3d7101ff6
aaanr0811 <aaanr0811@gmail.com>
2023-06-22 15:45:44 +0700

    first commit

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+@section('title', $viewData["title"])
+@section('content')
+<div class="card">
+    <div class="card-header">
+        Annisa Panel - Home Page
+    </div>
+    <div class="card-body">
+        Welcome to the Annisa Panel, use the sidebar to navigate between the different options.
+    </div>
+</div>
+@endsection
